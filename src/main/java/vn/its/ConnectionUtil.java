5254f30b45a364dc3420e2a7302059fd138ad965
tuannt39 <tuannt39@gmail.com>
2018-04-16 17:02:29 +0700

    improve coding design

---
+++ src/main/java/vn/its/ConnectionUtil.java
@@ -0,0 +1,26 @@
+package vn.its;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+public class ConnectionUtil {
+
+	private static SessionFactory sessionFactory = null;
+
+	public static SessionFactory getSessionFactory() {
+		if (sessionFactory == null) {
+			Configuration configuration = new Configuration();
+			configuration.configure();
+
+			ServiceRegistry registry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties())
+					.build();
+			sessionFactory = configuration.buildSessionFactory(registry);
+			return sessionFactory;
+		} else {
+			return sessionFactory;
+		}
+	}
+
+}
--- src/main/java/vn/its/Management.java
+++ src/main/java/vn/its/Management.java
@@ -11,21 +11,22 @@ import vn.its.model.Course;
 public class Management {
 
 	public static void main(String[] args) {
-//		Course course = new Course(4, "Jpa");
+		// Course course = new Course(4, "Jpa");
 
-		Configuration configuration = new Configuration();
-		configuration.configure();
-		
-		ServiceRegistry registry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
-		SessionFactory sessionFactory = configuration.buildSessionFactory(registry);
-		Session session = sessionFactory.openSession();
-		session.beginTransaction();
-//		session.save(course);
-//		session.getTransaction().commit();
-		Course hibernate = (Course) session.get(Course.class, 2);
-		System.out.println(hibernate);
-		sessionFactory.close();
-		
+		SessionFactory sessionFactory = ConnectionUtil.getSessionFactory();
+
+		try {
+			Session session = sessionFactory.openSession();
+			session.beginTransaction();
+			// session.save(course);
+			// session.getTransaction().commit();
+			Course hibernate = (Course) session.get(Course.class, 2);
+			System.out.println(hibernate);
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+		ConnectionUtil.getSessionFactory().close();
+
 	}
 
 }
